37189b3ccd5b02c65671f1843082533a1d631147
Kiran P <kk0509@pm.me>
2025-07-25 17:25:07 -0400

    add CLI args for all supported arguments

--- src/md2epub/__main__.py
+++ src/md2epub/__main__.py
@@ -5,11 +5,14 @@ def main():
     parser = argparse.ArgumentParser(description="Convert Markdown files to EPUB books.")
     parser.add_argument('md_source', type=str, help='Path to input Markdown file or directory containing Markdown files.')
     parser.add_argument('-o', '--output', type=str, help='Path to output EPUB file.', required=False)
+    parser.add_argument('-a', '--author', type=str, help='Name of the author', required=False)
+    parser.add_argument('-i', '--id', type=str, help='ID for the epub book', required=False)
+    parser.add_argument('-t', '--title', type=str, help='Title for the epub book', required=False)
 
     args = parser.parse_args()
 
     print(f"Converting '{args.md_source}' ...")
-    convert(md_source=args.md_source, epub_file=args.output)
+    convert(md_source=args.md_source, epub_file=args.output, book_author=args.author, book_id=args.id, book_title=args.title)
 
 
 if __name__ == "__main__":
